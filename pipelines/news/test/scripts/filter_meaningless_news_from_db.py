adf0b010fdcf7062d30ddf6cc2a20220914687d7
wambatcodeeee <feyfey927@gmail.com>
2026-07-19 17:31:48 +0900

    fix: 필터링 스크립트 실행경로 수정

--- pipelines/news/test/scripts/filter_meaningless_news_from_db.py
+++ pipelines/news/test/scripts/filter_meaningless_news_from_db.py
@@ -2,7 +2,7 @@ from pathlib import Path
 import argparse
 import sys
 
-PROJECT_ROOT = Path(__file__).resolve().parents[1]
+PROJECT_ROOT = Path(__file__).resolve().parents[3]
 if str(PROJECT_ROOT) not in sys.path:
     sys.path.insert(0, str(PROJECT_ROOT))
 
